f82c75bfb89817452e49b9d1c53bc788ead03a02
mhdatheek136 <87757769+mhdatheek136@users.noreply.github.com>
2024-01-28 18:07:01 +0500

    Completed testing class habit

--- test_habit.py
+++ test_habit.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from habit import Habit
 import pytest
 from freezegun import freeze_time
@@ -9,56 +10,139 @@ class TestHabit:
 
     def setup_method(self):
         self.db = get_db("test.db")
-        self.habit = Habit("driving", "daily", "Drive for 30 minutes")
-
-        add_habit(self.db, "coding", "daily", "Code for 60 minutes", "2023-12-01", 1)
-        add_habit(self.db, "exercise", "daily", "Exercise for 30 minutes", "2023-12-01", 0)
-        add_habit(self.db, "studying", "weekly", "Study for 60 minutes", "2023-12-01", 0)
-        add_habit(self.db, "reading", "weekly", "Read for 2 hours", "2023-12-01", 1)
-        add_habit(self.db, "swimming", "monthly", "Swim for 60 minutes", "2023-12-01", 1)
-        add_habit(self.db, "camping", "yearly", "Camp for 2 nights", "2023-12-01", 1)
-
-        update_log(self.db, "coding", 1, "2023-12-02")
-        update_log(self.db, "reading", 1, "2023-12-04")
-        update_log(self.db, "swimming", 1, "2023-12-06")
-        update_log(self.db, "camping", 1, "2023-12-08")
+        self.habit1 = Habit("driving", "daily", "Drive for 30 minutes")
+        self.habit2 = Habit("reading", "weekly", "Code for 30 minutes")
+        self.habit3 = Habit("swimming", "monthly", "Swim for 30 minutes")
+        self.habit4 = Habit("camping", "yearly", "Camp for 30 minutes")
 
     def test_create(self):
-        self.habit.create(self.db)
+        self.habit1.create(self.db)
         assert is_habit_exists(self.db, "driving") is True
 
     def test_delete(self):
-        self.habit.create(self.db)
-        self.habit.delete(self.db)
+        self.habit1.create(self.db)
+        self.habit1.delete(self.db)
         assert is_habit_exists(self.db, "driving") is False
 
     def test_edit_name(self):
-        self.habit.create(self.db)
-        self.habit.edit_name(self.db, "biking")
+        self.habit1.create(self.db)
+        self.habit1.edit_name(self.db, "biking")
         assert is_habit_exists(self.db, "biking") is True
         assert is_habit_exists(self.db, "driving") is False
 
     def test_edit_description(self):
-        self.habit.create(self.db)
-        self.habit.edit_description(self.db, "Drive for 1 hour")
+        self.habit1.create(self.db)
+        self.habit1.edit_description(self.db, "Drive for 1 hour")
         assert get_description(self.db, "driving") == "Drive for 1 hour"
 
     def test_change_periodicity(self):
-        self.habit.create(self.db)
-        self.habit.change_periodicity(self.db, "weekly")
+        self.habit1.create(self.db)
+        self.habit1.handle_streaks(self.db)
+        assert get_current_streak(self.db, "driving") == 1
+        assert get_last_update_date(self.db, "driving") == datetime.now().strftime("%Y-%m-%d")
+        self.habit1.change_periodicity(self.db, "weekly")
         assert get_periodicity(self.db, "driving") == "weekly"
+        assert get_current_streak(self.db, "driving") == 0
 
     def test_add_event(self):
-        self.habit.create(self.db)
-        self.habit.add_event(self.db)
+        self.habit1.create(self.db)
+        self.habit1.add_event(self.db)
         assert get_current_streak(self.db, "driving") == 1
         assert get_last_update_date(self.db, "driving") == datetime.now().strftime("%Y-%m-%d")
 
-    def test_handle_streak_when_last_event_date_None(self):
-        self.habit.create(self.db)
+    def test_handle_streak_when_last_event_date_none(self):
+        self.habit1.create(self.db)
+        self.habit1.handle_streaks(self.db)
+        assert get_current_streak(self.db, "driving") == 1
+        assert get_last_update_date(self.db, "driving") == datetime.now().strftime("%Y-%m-%d")
 
+    def test_handle_streak_daily(self):
+        frozen_date = datetime(2023, 12, 2)
+        with freeze_time(frozen_date):
+            self.habit1.create(self.db)
+            self.habit1.handle_streaks(self.db)
+            assert get_current_streak(self.db, "driving") == 1
+            assert get_last_update_date(self.db, "driving") == datetime.now().strftime("%Y-%m-%d")
+            self.habit1.handle_streaks(self.db)
+            assert get_current_streak(self.db, "driving") == 1
+        frozen_date = datetime(2023, 12, 3)
+        with freeze_time(frozen_date):
+            self.habit1.handle_streaks(self.db)
+            assert get_current_streak(self.db, "driving") == 2
+            assert get_last_update_date(self.db, "driving") == datetime.now().strftime("%Y-%m-%d")
+        frozen_date = datetime(2023, 12, 5)
+        with freeze_time(frozen_date):
+            self.habit1.handle_streaks(self.db)
+            assert get_current_streak(self.db, "driving") == 1
 
+    def test_handle_streak_weekly(self):
+        frozen_date = datetime(2023, 12, 1)
+        with freeze_time(frozen_date):
+            self.habit2.create(self.db)
+            self.habit2.handle_streaks(self.db)
+            assert get_current_streak(self.db, "reading") == 1
+            assert get_last_update_date(self.db, "reading") == datetime.now().strftime("%Y-%m-%d")
+            self.habit2.handle_streaks(self.db)
+            assert get_current_streak(self.db, "reading") == 1
+        frozen_date = datetime(2023, 12, 3)
+        with freeze_time(frozen_date):
+            self.habit2.handle_streaks(self.db)
+            assert get_current_streak(self.db, "reading") == 1
+        frozen_date = datetime(2023, 12, 4)
+        with freeze_time(frozen_date):
+            self.habit2.handle_streaks(self.db)
+            assert get_current_streak(self.db, "reading") == 2
+            assert get_last_update_date(self.db, "reading") == datetime.now().strftime("%Y-%m-%d")
+        frozen_date = datetime(2023, 12, 22)
+        with freeze_time(frozen_date):
+            self.habit2.handle_streaks(self.db)
+            assert get_current_streak(self.db, "reading") == 1
 
+    def test_handle_streak_monthly(self):
+        frozen_date = datetime(2023, 12, 4)
+        with freeze_time(frozen_date):
+            self.habit3.create(self.db)
+            self.habit3.handle_streaks(self.db)
+            assert get_current_streak(self.db, "swimming") == 1
+            assert get_last_update_date(self.db, "swimming") == datetime.now().strftime("%Y-%m-%d")
+            self.habit3.handle_streaks(self.db)
+            assert get_current_streak(self.db, "swimming") == 1
+        frozen_date = datetime(2023, 12, 27)
+        with freeze_time(frozen_date):
+            self.habit3.handle_streaks(self.db)
+            assert get_current_streak(self.db, "swimming") == 1
+        frozen_date = datetime(2024, 1, 10)
+        with freeze_time(frozen_date):
+            self.habit3.handle_streaks(self.db)
+            assert get_current_streak(self.db, "swimming") == 2
+            assert get_last_update_date(self.db, "swimming") == datetime.now().strftime("%Y-%m-%d")
+        frozen_date = datetime(2024, 3, 2)
+        with freeze_time(frozen_date):
+            self.habit3.handle_streaks(self.db)
+            assert get_current_streak(self.db, "swimming") == 1
+
+    def test_handle_streaks_yearly(self):
+        frozen_date = datetime(2023, 12, 4)
+        with freeze_time(frozen_date):
+            self.habit4.create(self.db)
+            self.habit4.handle_streaks(self.db)
+            assert get_current_streak(self.db, "camping") == 1
+            assert get_last_update_date(self.db, "camping") == datetime.now().strftime("%Y-%m-%d")
+            self.habit4.handle_streaks(self.db)
+            assert get_current_streak(self.db, "camping") == 1
+        frozen_date = datetime(2023, 12, 27)
+        with freeze_time(frozen_date):
+            self.habit4.handle_streaks(self.db)
+            assert get_current_streak(self.db, "camping") == 1
+        frozen_date = datetime(2024, 1, 10)
+        with freeze_time(frozen_date):
+            self.habit4.handle_streaks(self.db)
+            assert get_current_streak(self.db, "camping") == 2
+            assert get_last_update_date(self.db, "camping") == datetime.now().strftime("%Y-%m-%d")
+        frozen_date = datetime(2026, 3, 2)
+        with freeze_time(frozen_date):
+            self.habit4.handle_streaks(self.db)
+            assert get_current_streak(self.db, "camping") == 1
 
     def teardown_method(self):
         self.db.close()
